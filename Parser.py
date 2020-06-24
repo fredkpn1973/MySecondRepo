a26dde93f89f870817e473a8d7d1cc8bbe2f7dac
Fred Kok <fkok@vosko.nl>
2020-06-24 16:31:34 +0200

    values

--- Parser.py
+++ Parser.py
@@ -18,7 +18,7 @@ class ReSearcher():
 class Vividict(dict):
     def __missing__(self, key):
         value = self[key] = type(self)()
-        return value
+        return values
 
 
 def splitrange(raw_range):
